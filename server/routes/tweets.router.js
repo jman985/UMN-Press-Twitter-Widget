f2559282c07a6569bdd26c6dbdfca829efe7803d
Ross Hutchens <rlhcxm@gmail.com>
2020-09-01 09:24:07 -0500

    GET call rejects unauthenticated requests

--- server/routes/tweets.router.js
+++ server/routes/tweets.router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const {default: axios} = require('axios');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const token = process.env.BEARER_TOKEN;
 
 // GET from Twitter API
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     // console.log('preparing to hit server with a request to the Twitter API');
     axios.get(`https://api.twitter.com/2/tweets/search/recent?query=book1&max_results=10`, {
         headers: {
