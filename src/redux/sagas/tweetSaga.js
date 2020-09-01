ea7c2128b8cb73377198333749685afbe4096110
Paul Joachim <63604194+PaulDJoachim@users.noreply.github.com>
2020-09-01 13:30:25 -0500

    added for loop to tweet saga

--- src/redux/sagas/tweetSaga.js
+++ src/redux/sagas/tweetSaga.js
@@ -7,7 +7,7 @@ function* getTweets(action){
     for (let i=0; i<action.payload.length; i++){
         const response = yield axios.get('/tweets/' + action.payload[i].title)
         // send the response(tweet id) and the publication object from database to the save saga
-        yield put({ type: 'SAVE_TWEETS', payload: {tweetId: response.data, publicationId: action.payload[i].id}});
+        yield put({ type: 'SAVE_TWEETS', payload: {tweetArray: response.data, publicationId: action.payload[i].id}});
         console.log('sending this to save tweet saga:', response.data)
     }
   } catch (error) {
@@ -17,8 +17,13 @@ function* getTweets(action){
 
 function* saveTweets(action){
   try {
-    const response = yield axios.get('/tweets/save', action.payload);
-    console.log('sending this tweet to save route:', action.payload)
+    const tweets = action.payload.tweetArray
+    for (let tweet of tweets){
+      const tweetId = tweet.id
+      const publicationId = action.payload.publicationId
+      console.log('sending these to tweet save route:', {tweetId: tweetId, publicationId: publicationId})
+      // yield axios.post('/tweets/save', {tweetId: tweet.id, publicationId: action.payload.publicationId});
+    }
   } catch (error) {
       console.log('error with tweet save route', error);
   }
