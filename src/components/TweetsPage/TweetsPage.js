de79d412c21131ba08723f232f0f96fc0617c812
Paul Joachim <63604194+PaulDJoachim@users.noreply.github.com>
2020-09-03 10:59:37 -0500

    added flexbox styling and buttons to tweets

--- src/components/TweetsPage/TweetsPage.js
+++ src/components/TweetsPage/TweetsPage.js
@@ -9,20 +9,43 @@ class Publications extends Component {
     this.props.dispatch({type:'FETCH_DATABASE_TWEETS'});
   }
 
+  handleApprove = (tweetId) => {
+    this.props.dispatch({type:'APPROVE_TWEET', payload: tweetId})
+  }
+
+  handleRejection = (tweetId) => {
+    this.props.dispatch({type:'REJECT_TWEET', payload: tweetId})
+  }
+
   render() {
+    // prevent the DOM from rendering until the dbTweets array has loaded
     if (this.props.dbTweets.map === undefined) return null;
+    
+    // this portion filters all database tweets into an "undecided" array with only tweets
+    // that have approved value of null
+    let undecided = this.props.dbTweets.filter(function (filteredTweets) {
+      return filteredTweets.approved === null;
+    });
+
     return(
       <>
         <p>hello this is the tweets page</p>
-        {this.props.dbTweets.map((tweet, index) => (
+        <div class='box' style={{display: "flex",flexWrap: "wrap"}}>
+          {undecided.slice(0, 6).map((tweet, index) => (
+            <div style={{border: "solid"}}>
               <tr key={index}>
-                  <TwitterTweetEmbed key = {index} tweetId={tweet.tweet_id}
-                  options={{width: 250}}/>
-                <td>{tweet.tweet_id}</td>
-                <td>{tweet.publication_id}</td>
+                <TwitterTweetEmbed key = {index} tweetId={tweet.tweet_id}
+                options={{width: 250}}/>
+                <div style={{display: "flex", justifyContent: "space-around"}}>
+                  <button onClick={()=>this.handleApprove(tweet.id)}>Approve</button>
+                  <button onClick={()=>this.handleRejection(tweet.id)}>Reject</button>
+                </div>
+                {/* <td>{tweet.tweet_id}</td>
+                <td>{tweet.publication_id}</td> */}
               </tr>
-              )
-            )}
+            </div>
+          ))}
+        </div>
       </>
     )
   } 
