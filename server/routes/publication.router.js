24898f682bc287a0fb52a7e7d81e3376af048207
Ross Hutchens <rlhcxm@gmail.com>
2020-09-14 00:11:15 -0500

    Included Toggle Button on Publications to flip Include state for all Publications

--- server/routes/publication.router.js
+++ server/routes/publication.router.js
@@ -23,7 +23,7 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 });
 
 
-// updates the inclusion value of a publication
+
 router.put('/', rejectUnauthenticated, (req, res) => {
   console.log('Toggling inclusion state of publication #', req.body.id)
   const queryText = `
@@ -42,48 +42,25 @@ router.put('/', rejectUnauthenticated, (req, res) => {
   })
 })
 
-
-// updates the search_type value of a publication
-router.put('/searchtype', rejectUnauthenticated, (req, res) => {
-  console.log('Changing Searchtype of publication #', req.body.id)
+router.put('/all', rejectUnauthenticated, (req, res) => {
+  console.log('Toggling inclusion state of all publications')
   const queryText = `
   UPDATE publication
-  SET search_type = $1
-  WHERE id = $2
+  SET include = $1
   `
-  const values = [req.body.searchType, req.body.id]
-  pool.query(queryText, values)
+  pool.query(queryText, [req.body.include])
   .then( (response) => {
-      console.log( 'Successfully changed Searchtype');
+      console.log( 'Successfully toggled inclusion state');
       res.send(response.rows);
   })
   .catch( (err) => {
-      console.log('An error occured while trying to change search type:', err);
+      console.log('An error occured while toggling inclusion state:', err);
       res.sendStatus(500);
   })
 })
-
-// updates the search_type value of all publications
-router.put('/searchtypeall', rejectUnauthenticated, (req, res) => {
-  console.log('Changing Searchtype of all publications to ', req.body.searchType)
-  const queryText = `
-  UPDATE publication
-  SET search_type = $1
-  `
-  const values = [req.body.searchType]
-  pool.query(queryText, values)
-  .then( (response) => {
-      console.log( 'Successfully changed all Searchtypes');
-      res.send(response.rows);
-  })
-  .catch( (err) => {
-      console.log('An error occured while changing all search types:', err);
-      res.sendStatus(500);
-  })
-})
-
 
 //update timestamp 
+
 router.put('/timestamp/:id', rejectUnauthenticated, (req, res) => {
   console.log('update last_searched timestamp');
     pool.query(`UPDATE "publication" 
@@ -99,27 +76,14 @@ router.put('/timestamp/:id', rejectUnauthenticated, (req, res) => {
 /**
  * POST route template
  */
-router.post("/csv", async (req, res) => {
+router.post('/csv', rejectUnauthenticated, async (req, res) => {
   const csvData = req.body.payload;
-  //console.log("OOOOO", csvData);
   const connection = await pool.connect();
   const notAvailable = "not provided";
 
   try {
-    // for (book of csvData) {
-    //   if (book.data.title === undefined) {
-    //     console.log("pooooop");
-    //   } else {
-    //     console.log(book.data.title);
-    //   }
-    // }
     await connection.query("BEGIN");
-
-    const queryText = `INSERT INTO "publication" ("title", "author1", "subtitle") 
-    SELECT CAST($1 AS VARCHAR(1000)) AS "title", CAST($2 AS VARCHAR(1000)) AS "author1", CAST($3 AS VARCHAR(1000)) AS "subtitle"
-     WHERE NOT EXISTS (SELECT * FROM "publication" WHERE "title" = $1 AND "author1" = $2 AND "subtitle" = $3);`;
-
-    // const queryText = `INSERT INTO "publication" ("title", "author1", "subtitle") VALUES ($1, $2, $3);`;
+    const queryText = `INSERT INTO "publication" ("title", "author1", "subtitle") VALUES ($1, $2, $3);`;
 
     for (book of csvData) {
       await connection.query(queryText, [
