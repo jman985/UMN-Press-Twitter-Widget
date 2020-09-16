0739bb0b72e342e986e386e97a446ec143bd7e30
Josh Maus <jman985@gmail.com>
2020-09-16 15:48:32 -0500

    removed unused route

--- server/routes/widget.router.js
+++ server/routes/widget.router.js
@@ -22,29 +22,6 @@ router.get( '/:publication_id', ( req, res )=>{
 })
 
 
-// GET Tweet from Twitter Embed API
 
-router.get('/:publication_id/:tweet_id', (req, res) => {
-    console.log( 'in router /api/tweets GET embed html', req.params );
-    
-    axios.get(`https://publish.twitter.com/oembed?url=https://twitter.com/anyuser/status/${req.params.tweet_id}`)
-
-        .then((response)=>{
-            console.log('sending back:', response.data.html);
-            res.send(response.data.html);
-        })
-        .catch((error)=>{
-            console.log('error with Twitter GET', error);
-            res.sendStatus(500);
-        });
-})
-
-
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-
-});
 
 module.exports = router;
